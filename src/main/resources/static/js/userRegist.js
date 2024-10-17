b972daf8821bf6ce29209322e9862a7dc4cc79ea
fugo8015276 <sf.engineer20231201@gmail.com>
2024-10-17 23:29:08 +0900

    補足説明を加えてコミット
    補足説明を加えてコミット
    後日改造して動作確認を行う。

--- src/main/resources/static/js/userRegist.js
+++ src/main/resources/static/js/userRegist.js
@@ -5,9 +5,11 @@ $(function() {
 	$("#registBtn").click(function() {
 		var elmErrorMsg = $("#errorMsg");
 		elmErrorMsg.text("");
+		//false(入力値エラー)だった場合、falseを返却。
 		if (!inputCheck()) {
 			return false;	
 		}
+		
 		
 		$.ajax({
 			type:  "post",
@@ -15,18 +17,27 @@ $(function() {
 			data:  $("form").serialize(),
 			dataType:"json",
 		})
+		//正常終了した際の処理。
+		//dataの中にControllerクラスから渡された情報（JSON）が入っている。
 		.done(function(data) {
+			//data内のuserListがnullの場合
 			if (data.userList == null) {
+				//エラーメッセージを出力。
 				elmErrorMsg.text(data.errorMsg);
+				//後続の処理を辞める。
 				return false;
 			}
 			
 			var viewUser = "";
+			//userListが空でなかった場合、userListの情報を出力
 			data.userList.forEach((user, index) => {
+				//indexはその場の番号　userID = ユーザーID　userName = ユーザー名
 				viewUser = viewUser + index + " : " + user.userId + "," + user.userName + "<br>";
 			});
+			//Ajaxリクエストが成功した後に、userList というIDを持つHTML要素の中身を動的に更新するためのコード
 			$("#userList").html(viewUser);
 		})
+		//HTTPステータスが200(成功)以外で帰ってきた場合に実行
 		.fail(function() {
 			elmErrorMsg.text("※予期せぬエラーが発生しました");
 		});
@@ -39,7 +50,7 @@ $(function() {
 function inputCheck() {
 	var elmErrorMsg = $("#errorMsg");
 	var userId = $("#userId").val();
-	if (userId.length == 0) {
+	if (userId.length == 0) {//入力値がなかった時エラーメッセージを出力する。
 		elmErrorMsg.text("※ユーザーIDが入力されていません。");
 		return false;
 	}
@@ -49,6 +60,6 @@ function inputCheck() {
 		elmErrorMsg.text("※ユーザー名が入力されていません。");
 		return false;
 	}
-	
+	//両方とも値が入っている場合、trueを返却。
 	return true;
 }
